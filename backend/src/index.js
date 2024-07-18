40171a94b8f1789e5133aae4f91bdb95a4d2d011
Angga Suriana <anggasurianae65@gmail.com>
2024-07-18 18:21:21 +0000

    feature: add api key authorization

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,19 @@
 const fastify = require('fastify')({ logger: { level: 'info', prettifier: require('pino-pretty') } });
 const port = 4501
 const host = '0.0.0.0'
+const apiKey = 'YTdlMjAxZDYyZGY4Yzk2OTkzNjc5NjU1YTRkMTY2NzQ3ODU1YzBhMA';
+
+const apiKeyValidation = async (req, res) => {
+  const authorization = req.headers['authorization'];
+
+  if (!authorization || authorization !== `ZeroOneGroup ${apiKey}`) {
+    res.code(404).send({ message: 'Route ' + req.method + ':' + req.url + ' not found', error: 'Not Found', statusCode:404 });
+    return;
+  }
+
+};
+
+fastify.addHook('preHandler', apiKeyValidation);
 
 fastify.get('/api/v1/hello_world/', (req, res) => {
   return { message: 'Hello World!', statusCode:200 };
