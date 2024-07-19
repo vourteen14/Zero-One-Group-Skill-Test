9c1b018c95e857d8779361464f23c755b7fc5f57
Angga Suriana <anggasurianae65@gmail.com>
2024-07-19 01:55:12 +0000

    feature: add logo and implement SSR

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import axios from 'axios';
 
-export default function Home() {
+const Index = ({ message }) => {
   return (
     <>
       <Head>
@@ -11,8 +12,38 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
       </Head>
       <main>
-
+        <div style={{ marginTop: '100px' }}>
+          <Image 
+            src="/logo.svg" 
+            width={200} 
+            height={200}
+          />
+        </div>
+        <h4>Response from backend:</h4>
+        <h2>{ message }</h2>
       </main>
     </>
   )
 }
+
+export async function getServerSideProps() {
+  try {
+    const res = await axios.get(`${process.env.BACKEND_API_URL}/api/v1/hello_world/`, { headers: { 'Authorization': `ZeroOneGroup ${process.env.BACKEND_AUTH_TOKEN }` }});
+    const data = res.data;
+
+    return {
+      props: {
+        message: data.message,
+      },
+    };
+
+  } catch (error) {
+    return {
+      props: {
+        message: 'Backend connection failed',
+      },
+    };
+  }
+}
+
+export default Index;
